3a50113abe1c42024e21e6233fb91634e31f0fcd
Rajan Kumar <rajanyadav1509@gmail.com>
2023-08-16 13:02:41 +0530

    commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".pev-btn",
+        nextArrow:".next-btn",
+
+        
+    });
+});
